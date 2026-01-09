78bd964e844a17a7ef7df714c2b5681a1ae70aea
ZhongYT <768119359@qq.com>
2026-01-09 15:40:53 +0800

    feat: web端-接口返回统一格式

--- demo/nest-login/src/main.ts
+++ demo/nest-login/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './core/filter/http-exception/http-exception.filter';
+import { TransformInterceptor } from './core/interceptor/transform/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // 注册全局错误的过滤器
+  app.useGlobalFilters(new HttpExceptionFilter());
+  // 全局注册拦截器
+  app.useGlobalInterceptors(new TransformInterceptor());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
