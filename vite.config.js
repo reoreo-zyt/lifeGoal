ee937001bd2b8280bf5760f8ba9873e61b5b8a0c
ZhongYT <768119359@qq.com>
2026-03-26 11:34:08 +0800

    feat: 打包修复

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: './',
-})
+  build: {
+    target: "es2015", // 或者其他你需要的目标环境
+  },
+  base: "./",
+});
