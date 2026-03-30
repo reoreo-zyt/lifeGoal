9f8f211b275018178d149d813e22a54ad3bf42a3
ZhongYT <768119359@qq.com>
2026-03-30 16:51:57 +0800

    feat: 数据库

--- backend/src/database/database.module.ts
+++ backend/src/database/database.module.ts
@@ -10,7 +10,7 @@ import { ConfigModule } from '@nestjs/config';
       host: process.env.DB_HOST || 'localhost',
       port: parseInt(process.env.DB_PORT, 10) || 3306,
       username: process.env.DB_USERNAME || 'root',
-      password: process.env.DB_PASSWORD || 'password',
+      password: process.env.DB_PASSWORD || '1540689086Zyt@',
       database: process.env.DB_NAME || 'lifegoal',
       entities: [__dirname + '/../**/*.entity{.ts,.js}'],
       synchronize: true,
